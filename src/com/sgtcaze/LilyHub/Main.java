1176ab3d237e864f02223f18d51c6e6f84c936c3
sgtcaze <sgtcaze@outlook.com>
2013-09-25 22:46:43 -0700

    Now uses LilyPad Connect API + /lobby

--- src/com/sgtcaze/LilyHub/Main.java
+++ src/com/sgtcaze/LilyHub/Main.java
@@ -1,5 +1,9 @@
 package com.sgtcaze.LilyHub;
 
+import lilypad.client.connect.api.Connect;
+import lilypad.client.connect.api.request.RequestException;
+import lilypad.client.connect.api.request.impl.RedirectRequest;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -7,18 +11,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin {
+	
+	Connect connect;
 
 	public void onEnable() {
+		this.connect = ((Connect)getServer().getServicesManager().getRegistration(Connect.class).getProvider());
 		saveDefaultConfig();
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
-		Player player = (Player) sender;
-		if (cmd.getName().equalsIgnoreCase("hub")) {
-			    String server = getConfig().getString("Server");
-				player.performCommand("server " + server);
+		if (cmd.getName().equalsIgnoreCase("hub") || cmd.getName().equalsIgnoreCase("lobby")) {
+			    try {
+			        this.connect.request(new RedirectRequest(getConfig().getString("Server"), ((Player)sender).getName()));
+			      } catch (RequestException e) {
+			        e.printStackTrace();
+			      }
+			      return true;
 			}
 		return true;
 

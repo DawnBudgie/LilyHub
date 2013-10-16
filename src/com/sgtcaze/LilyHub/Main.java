3f1c567f1e489564f4604a83270478e48021aac7
sgtcaze <sgtcaze@outlook.com>
2013-10-15 22:38:20 -0700

    Cleanup source

--- src/com/sgtcaze/LilyHub/Main.java
+++ src/com/sgtcaze/LilyHub/Main.java
@@ -9,27 +9,29 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class Main extends JavaPlugin {
-	
+
 	Connect connect;
 
 	public void onEnable() {
-		this.connect = ((Connect)getServer().getServicesManager().getRegistration(Connect.class).getProvider());
+		this.connect = ((Connect) getServer().getServicesManager()
+				.getRegistration(Connect.class).getProvider());
 		saveDefaultConfig();
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
-		if (cmd.getName().equalsIgnoreCase("hub") || cmd.getName().equalsIgnoreCase("lobby")) {
-			    try {
-			        this.connect.request(new RedirectRequest(getConfig().getString("Server"), ((Player)sender).getName()));
-			      } catch (RequestException e) {
-			        e.printStackTrace();
-			      }
-			      return true;
+		if (cmd.getName().equalsIgnoreCase("hub")
+				|| cmd.getName().equalsIgnoreCase("lobby")) {
+			try {
+				this.connect.request(new RedirectRequest(getConfig().getString(
+						"Server"), ((Player) sender).getName()));
+			} catch (RequestException e) {
+				e.printStackTrace();
 			}
+			return true;
+		}
 		return true;
 
 	}

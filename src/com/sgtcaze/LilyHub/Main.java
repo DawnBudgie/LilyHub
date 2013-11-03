5398d6d6cdddfd99db3854e5ae80495ef3547921
sgtcaze <sgtcaze@outlook.com>
2013-11-02 19:23:47 -0700

    Added cooldowns and version update

--- src/com/sgtcaze/LilyHub/Main.java
+++ src/com/sgtcaze/LilyHub/Main.java
@@ -13,26 +13,54 @@ public class Main extends JavaPlugin {
 
 	Connect connect;
 
+	public double timer = 1;
+
 	public void onEnable() {
 		this.connect = ((Connect) getServer().getServicesManager()
 				.getRegistration(Connect.class).getProvider());
+
 		saveDefaultConfig();
+
+		this.timer = getConfig().getDouble("Delay");
+	}
+
+	public void redirect(String string, Player player) {
+		try {
+			connect.request(new RedirectRequest(
+					getConfig().getString("Server"), player.getName()));
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
+		final Player player = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("hub")
 				|| cmd.getName().equalsIgnoreCase("lobby")) {
-			try {
-				this.connect.request(new RedirectRequest(getConfig().getString(
-						"Server"), ((Player) sender).getName()));
-			} catch (RequestException e) {
-				e.printStackTrace();
-			}
-			return true;
-		}
-		return true;
+			if (getConfig().getBoolean("TeleportDelay")) {
+				if (player.hasPermission("lilyhub.bypass")) {
+					redirect(getConfig().getString("Server"), player);
+				} else {
+					getServer().getScheduler().scheduleSyncDelayedTask(this,
+							new Runnable() {
+								public void run() {
+									try {
+										connect.request(new RedirectRequest(
+												getConfig().getString("Server"),
+												player.getName()));
+									} catch (RequestException e) {
+										e.printStackTrace();
+									}
+								}
+							}, (long) (this.timer * 20));
+				}
+			} else {
+				redirect(getConfig().getString("Server"), player);
 
+			}
+		}
+		return false;
 	}
 }
